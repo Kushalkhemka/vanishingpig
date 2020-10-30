3fe3d050eef45c69a4ee42c34b181a20a88dcc50
Kushalkhemka <Kushalkhemka559@gmail.com>
2020-10-30 18:54:49 +0530

    modified

--- Pig.js
+++ Pig.js
@@ -2,6 +2,26 @@ class Pig extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
     this.image = loadImage("sprites/enemy.png");
+    this.Visibility=255;
+  }
+
+  display()
+  {
+    if(this.body.speed>3)
+    {
+      World.remove(world,this.body);
+      push();
+      this.Visibility=this.Visibility-5;
+      tint(255,this.Visibility);
+      var pos=this.body.position
+      image(this.image,pos.x,pos.y,50,50);
+      pop();
+    }
+    else
+    {
+      super.display();
+    }
+    
   }
 
 };
